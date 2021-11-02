c8a7a354c2894ab35f82eb139e9c7bc78b973e6f
Andrei Duque Lopes <andreiduquelopes@gmail.com>
2021-11-02 19:27:27 -0300

    Fix project configuration

--- src/v1/config/mongodb.ts
+++ src/v1/config/mongodb.ts
@@ -1,14 +1,11 @@
 import { createConnection } from "typeorm";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
-const notIsPrd = process.env.NODE_ENV !== "production";
-
-// eslint-disable-next-line @typescript-eslint/naming-convention
 export const connect = () =>
 	createConnection({
 		type: "mongodb",
 		url: process.env.MONGODB_URL,
 		synchronize: false,
-		logging: notIsPrd,
+		logging: process.env.NODE_ENV !== "production",
 		entities: [],
 	});
