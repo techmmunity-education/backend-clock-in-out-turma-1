0d1aaff8beba4f5a82c070ced9e24f5d2de19148
Igor Feitosa de Araujo <igorlavras@hotmail.com>
2021-11-03 09:45:08 -0300

    :sparkles: Add employe entity

--- src/v1/config/mongodb.ts
+++ src/v1/config/mongodb.ts
@@ -1,6 +1,7 @@
 import { createConnection } from "typeorm";
 import { ClockInOutEntity } from "v1/api/clock-in-out/clock-in-out.entity";
 import { CompanyEntity } from "v1/api/company/company.entity";
+import { EmployeeEntity } from "v1/api/employee/employee.entity";
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export const connect = () =>
@@ -9,5 +10,5 @@ export const connect = () =>
 		url: process.env.MONGODB_URL,
 		synchronize: false,
 		logging: process.env.NODE_ENV !== "production",
-		entities: [CompanyEntity, ClockInOutEntity],
+		entities: [CompanyEntity, ClockInOutEntity, EmployeeEntity],
 	});
---
+++ src/v1/enum/role-types.ts
@@ -0,0 +1,5 @@
+export enum RoleTypeEnum {
+	COMPANY = "COMPANY",
+	RH = "RH",
+	EMPLOYEE = "EMPOLYEE",
+}
